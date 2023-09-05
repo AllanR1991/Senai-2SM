81811ab9519b39b59a372b5475623a007d42e2d8
Allan Rodrigues dos Santos <allanrodrigues1991.ar@gmail.com>
2023-09-05 14:01:29 -0300

    Iniciando API

---
+++ API/senai_filmes_webApi/senai_filmes_webApi/Interfaces/IFilmeReporsitory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace senai_filmes_webApi.Interfaces
+{
+    public interface IFilmeReporsitory
+    {
+    }
+}
---
+++ API/senai_filmes_webApi/senai_filmes_webApi/Interfaces/IGeneroRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace senai_filmes_webApi.Interfaces
+{
+    /// <summary>
+    /// Interface responsavel pelo repositório GeneroRepository
+    /// Contem os metodos de genero
+    /// </summary>
+    public interface IGeneroRepository //se não colar public automaticamente é private.
+    {
+        // TipoRetorno NomeMetodo(TipoParametro parametro);
+        
+    }
+}
